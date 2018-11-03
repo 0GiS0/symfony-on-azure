57b6598404d1901aeb8a5aa0128aecaafecdee34
Gisela Torres <giselatb@outlook.com>
2018-11-03 19:27:44 +0100

    changed .htaccess

--- post-deployment.sh
+++ post-deployment.sh
@@ -1,13 +1,5 @@
-if [ -f  composer.json ] 
-    then 
-        echo "Found composer.json" 
-	    composer install 
-    else 
-	    echo "composer.json not found. Skipping composer install" 
-fi
-
+composer install
 echo "npm install launched"
 npm install
 echo "webpack launched"
 npm run-script build
-   
