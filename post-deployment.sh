0af8773f1c0c1637680ed036d9aea36c9c2407cd
Gisela Torres <giselatb@outlook.com>
2018-11-03 17:40:58 +0100

    fixed post-deployment.sh again

--- post-deployment.sh
+++ post-deployment.sh
@@ -6,13 +6,8 @@ if [ -f  composer.json ]
 	    echo "composer.json not found. Skipping composer install" 
 fi
 
-if [ -f  package.json ] 
-    then 
-        echo "Found package.json" 
-        echo "npm install launched" 
-        npm install
-        echo "webpack launched"
-        npm run-script build
-    else 
-	    echo "package.json not found. Skipping npm install" 
-fi
+echo "npm install launched"
+npm install
+echo "webpack launched"
+npm run-script build
+   
